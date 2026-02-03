0864c1c8473a6b515210f3c2a4f9a24f5991b59d
Vlad <vladislavnechaiev@gmail.com>
2026-02-03 18:28:31 +0200

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/timur-co' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/ConstructionDemoWebsite' : '',
 };
 
 export default nextConfig;
